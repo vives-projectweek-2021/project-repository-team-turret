097a6a146457eca029004988b322b7d5615d2c50
DevNode <thomasluca008@gmail.com>
2021-04-29 23:50:17 +0200

    made control for keypresses and updated filesystem

---
+++ frontend/src/keyboardControls.js
@@ -0,0 +1,32 @@
+const turretAngle = document.getElementById("turret-angle")
+
+var angle = 0
+const ENTER = 13
+const ARROW_RIGHT = 39
+const ARROW_LEFT = 37
+
+window.onload = function(){
+    
+
+    window.onkeydown= function(gfg){
+        if(gfg.keyCode === ARROW_RIGHT && isValidAngle(10)){
+            changeAngle(10)
+        };
+        if(gfg.keyCode === ARROW_LEFT && isValidAngle(-10))
+        {
+            changeAngle(-10)
+        };
+    };
+}
+
+function isValidAngle(delta) {
+    angle = Number(turretAngle.innerHTML.slice(0, -1))
+    return !(Math.abs(angle + delta) > 90)
+}
+
+function changeAngle(delta) {
+    angle += delta
+    turretAngle.innerHTML = angle + '°';
+    formatWsMsg("movement/vertical", String(angle))
+}
+
---
+++ frontend/src/websocket.js
@@ -0,0 +1,26 @@
+const ws = new WebSocket("ws://localhost:3000")
+
+ws.onopen = () => {
+    console.log("Connected via websocket")
+    ws.send(JSON.stringify({"message": "Client connected"}))
+}
+
+ws.onmessage = (message) => {
+    console.log(message.data)
+}
+
+ws.onclose = (event) => {
+    if (event.wasClean) {
+      alert(`Lost connection to WebSocket, code=${event.code} reason=${event.reason}`)
+    } else {
+      alert('Lost connection to WebSocket')
+    }
+}
+
+
+function formatWsMsg(topic, command) {
+    ws.send(JSON.stringify({
+        "topic": topic,
+        "command": command
+    }))
+}
