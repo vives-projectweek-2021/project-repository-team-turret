71157bf42f66927ff2a1a2f5de4d77ffa55789ab
DevNode <thomasluca008@gmail.com>
2021-04-30 11:35:24 +0200

    added .env for backend

--- backend/src/lib/mqttClient.js
+++ backend/src/lib/mqttClient.js
@@ -1,7 +1,8 @@
 const mqtt = require('mqtt')
+require('dotenv').config()
 const client = "commander" + Math.round((Math.random() * 1000))
 console.log(client)
-const mqttClient  = mqtt.connect(`mqtt://172.16.101.191`, {clientId:client})
+const mqttClient  = mqtt.connect(`mqtt:${process.env.BROKER_IP}`, {clientId:client})
 
 const baseTopic="tommys_blaster"
 const topicListener = baseTopic + "/#"
