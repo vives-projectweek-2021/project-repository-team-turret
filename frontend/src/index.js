5818acd860b693aeb8fa9ccca6dc8bbf3e880127
DevNode <thomasluca008@gmail.com>
2021-05-08 16:20:12 +0200

    added dynamic modi button styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,11 @@ const rightBtn = document.getElementById("right")
 
 const powerSlider = document.getElementById("power")
 
-const manualMode = document.getElementById("manual")
-const halfAutoMode = document.getElementById("half-auto")
-const autoMode = document.getElementById("auto")
-const defenseMode = document.getElementById("defense")
-
 const coordsP = document.getElementById("coords")
 const stream = document.getElementById("stream")
+
+const manualButton = document.getElementById("manual")
+const automaticButton = document.getElementById("automatic")
 
 fireBtn.addEventListener("click", () => {
     formatWsMsg("movement/fire", "1")
@@ -44,4 +42,18 @@ stream.addEventListener("mousedown", (e) => {
     angle = xRel
     turretAngle.innerHTML = angle + '°';
     formatWsMsg("movement/vertical", String(xRel))
+})
+
+
+const activeModeStyle = "background: #008779; border-color: #008779; color: white;"
+const inactiveModeStyle = "background: white; border-color: #e0e0e0; color: black;"
+
+automaticButton.addEventListener("click", () => {
+    automaticButton.style.cssText = activeModeStyle
+    manualButton.style.cssText = inactiveModeStyle
+})
+
+manualButton.addEventListener("click", () => {
+    manualButton.style.cssText = activeModeStyle
+    automaticButton.style.cssText = inactiveModeStyle
 })
